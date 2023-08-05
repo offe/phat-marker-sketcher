62d672f73fe57f7d43812e8ff10a4c6a260e895a
Oscar Lindberg <oscar.lindberg@gmail.com>
2023-08-05 12:33:38 +0200

    Some cleanup

--- phat-marker-sketcher/src/components/ButtonAppBar.js
+++ phat-marker-sketcher/src/components/ButtonAppBar.js
@@ -3,14 +3,18 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
-const buttons = ["Load project", "Save project"];
+const buttons = ["Load project", "New project", "Save project"];
 
 export default function ButtonAppBar() {
   return (
     <Box style={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
+          <Typography variant="h6" sx={{ my: 2, mr: 3 }}>
+            🖊️ Phat Marker Sketcher
+          </Typography>
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {buttons.map((item) => (
               <Button key={item} sx={{ color: "#fff" }}>
